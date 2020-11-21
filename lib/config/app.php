cc11753e3dbc2cd4d2d92a945f07105842d515d1
vofka <vofka@webasyst.com>
2020-11-21 13:25:57 +0300

    sash color

--- lib/config/app.php
+++ lib/config/app.php
@@ -3,9 +3,10 @@ return array(
     'name'    => 'Tasks',
     'icon'    => 'img/tasks-512.png',
     'version' => '1.3.0',
-    'vendor'  => 'webasyst',
+    'vendor'  => '1021997',
     'rights'  => true,
     'plugins' => true,
     'csrf'    => true,
     'license'     => 'commercial',
+    'sash_color' => '#e81a3d'
 );
