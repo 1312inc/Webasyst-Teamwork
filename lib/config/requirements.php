8f8f675a74abd38e71230dfb2b8f198ae1c8ad12
vofka <vofka@webasyst.com>
2021-05-11 13:51:28 +0300

    multiple ui tweaks

--- lib/config/requirements.php
+++ lib/config/requirements.php
@@ -5,7 +5,7 @@ return array(
         'version' => '>=7.1',
     ),
     'app.installer' => array(
-        'version' => '>=2.0.0',
+        'version' => '>=2.1.0',
         'strict' => true,
     ),
 );
