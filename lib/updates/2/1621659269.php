095fd833bba2e8a6ad05c964cbc1e0534c69d654
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-06-29 22:45:05 +0300

    9.161:Tasks update to 2.0 force updated all tasks 'update_datetime'

--- lib/updates/2/1621659269.php
+++ lib/updates/2/1621659269.php
@@ -3,6 +3,9 @@ $m = new tasksTaskModel();
 
 foreach ($m->getAll() as $task) {
     if (empty($task['uuid'])) {
-        $m->updateById($task['id'], ['uuid' => tasksUuid4::generate()]);
+        $m->exec(
+            'update tasks_task set uuid = s:uuid where id = i:id',
+            ['id' => $task['id'], 'uuid' => tasksUuid4::generate()]
+        );
     }
 }
