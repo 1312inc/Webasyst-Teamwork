7ccd6b14f116941a3f0642f9df23dc30e6c1ccb7
vofka <vofka@webasyst.com>
2021-07-21 12:08:37 +0300

    widgets pre-release config & ui finetune

--- widgets/actors/lib/config/widget.php
+++ widgets/actors/lib/config/widget.php
@@ -4,6 +4,6 @@ return array(
     'name' => /*_wp*/('Tasks actors'),
     'size' => array('2x2'),
     'img' => 'img/actors.png',
-    'version'=>'1.0',
-    'vendor' => 'webasyst',
+    'version'=>'1.1.0',
+    'vendor' => '1021997',
 );
