4b523e2018f6129cfb4eab18cd92cc7707d3576f
vofka <vofka@webasyst.com>
2021-07-29 13:07:08 +0300

    plugins version up

--- plugins/export/lib/config/plugin.php
+++ plugins/export/lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name'            => 'Export',
     'description'     => 'Exports task list to a CSV (Excel) file',
     'img'             => 'img/export.png',
-    'version'         => '1.0.0',
+    'version'         => '1.0.1',
     'vendor'          => '1021997',
     'custom_settings' => 0,
     'handlers'        => array(
