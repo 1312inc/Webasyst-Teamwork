c16c77e1ecc5c03375f8f4403eed3e3821a859b5
fixiel <artyom.yurov@webasyst.ru>
2021-09-01 19:12:01 +0300

    tasks/plugins/releases: график накопительной диаграммы потока CFD #95.130

--- plugins/releases/lib/tasksReleases.plugin.php
+++ plugins/releases/lib/tasksReleases.plugin.php
@@ -100,6 +100,7 @@ class tasksReleasesPlugin extends waPlugin
         wa()->getResponse()->addJs('plugins/releases/js/tasksScopeStats.js?v=' . $version, 'tasks');
         wa()->getResponse()->addJs('plugins/releases/js/taskKanbanSettings.js?v=' . $version, 'tasks');
         wa()->getResponse()->addCss('plugins/releases/css/style.css?v=' . $version, 'tasks');
+        wa()->getResponse()->addJs('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js?v=' . $version);
     }
 
     public function tasksCollectionSearch($params)
