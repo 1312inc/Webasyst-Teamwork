05e70a5d97adf0998731090bb747e1cc1c636981
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-09-07 11:48:56 +0300

    9.290:tasks.statuses.getList to contain 'params' for Open & Closed system statuses

--- lib/classes/Api/Status/GetList/tasksApiStatusGetListHandler.class.php
+++ lib/classes/Api/Status/GetList/tasksApiStatusGetListHandler.class.php
@@ -10,12 +10,12 @@ final class tasksApiStatusGetListHandler
         $statusesData = tasksHelper::getStatuses(null, false);
 
         $statusesData[tasksStatusModel::STATUS_OPEN_ID]['params'] = [
-            'button_color' => '#22d13d',
-            'title_color' => '#ffffff',
+            'button_color' => '22d13d',
+            'title_color' => 'ffffff',
         ];
         $statusesData[tasksStatusModel::STATUS_CLOSED_ID]['params'] = [
-            'button_color' => '#888888',
-            'title_color' => '#ffffff',
+            'button_color' => '888888',
+            'title_color' => 'ffffff',
         ];
 
         return $statusesData;
