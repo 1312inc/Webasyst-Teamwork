592a6962b562869acf17d0c6da83a1fcdc998462
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-09-10 10:14:30 +0300

    3.6:TEST COMMIT

--- lib/classes/Api/tasksApiResponseInterface.class.php
+++ lib/classes/Api/tasksApiResponseInterface.class.php
@@ -8,6 +8,9 @@ interface tasksApiResponseInterface
     public const HTTP_OK = 200;
     public const HTTP_FAIL = 400;
 
+    public const RESPONSE_OK = 'ok';
+    public const RESPONSE_FAIL = 'fail';
+
     /**
      * @return int
      */
