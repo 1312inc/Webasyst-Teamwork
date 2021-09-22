c46a1e756d322b312633c31f5d4b41c988f0ae27
vofka <vofka@webasyst.com>
2021-09-22 16:59:04 +0300

    9.289 checking git plugin integration (& T version up shtobi dva raza ne vstavatt)

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name'    => 'Tasks',
     'icon'    => 'img/tasks-512.png',
-    'version' => '2.1.1',
+    'version' => '2.1.3',
     'vendor'  => '1021997',
     'rights'  => true,
     'plugins' => true,
