2625be0a6047a116f58584032c44b59562d57c54
vofka <vofka@webasyst.com>
2021-10-28 12:47:39 +0300

    tasks widgets 1.1.1 version up

--- widgets/scopes/lib/config/widget.php
+++ widgets/scopes/lib/config/widget.php
@@ -4,6 +4,6 @@ return array(
     'name' => /*_wp*/('Scopes'),
     'size' => array('2x2'),
     'img' => 'img/scopes.png',
-    'version'=>'1.1.0',
+    'version'=>'1.1.1',
     'vendor' => '1021997',
 );
