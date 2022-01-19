7bf436d903003850e425fc189e0059b7c81e3e21
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-01-19 10:18:49 +0300

    9.560: archived projects > add 'Unarchive' button

---
+++ lib/actions/projects/tasksProjectsUnarchive.controller.php
@@ -0,0 +1,16 @@
+<?php
+
+class tasksProjectsUnarchiveController extends waJsonController
+{
+    public function execute()
+    {
+        $project_id = waRequest::post('id', 0, 'int');
+
+        if ($this->getUser()->getRights('tasks', 'project.'.$project_id) < tasksRights::PROJECT_ACCESS_FULL) {
+            throw new waRightsException(_ws('Access denied'));
+        }
+
+        $project_model = new tasksProjectModel();
+        $project_model->updateById($project_id, array('archive_datetime' => null));
+    }
+}
