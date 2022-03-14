1e4bdf49b3e09258ef6e9ea888a6a630d61f2a71
vofka <vofka@webasyst.com>
2022-03-14 13:27:09 +0300

    actors widget ui & loc

--- widgets/actors/lib/config/widget.php
+++ widgets/actors/lib/config/widget.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'name' => /*_wp*/('Teammates'),
+    'name' => /*_wp*/('Top assignees'),
     'size' => array('2x2'),
     'img' => 'img/actors.png',
     'version'=>'2.0.0',
