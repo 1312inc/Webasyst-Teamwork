4bfe857174bf253775ff2fa65a51db8aca2957be
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-05-24 19:11:08 +0300

    9.854: enable utf8mb4 for the Tasks app

--- lib/config/install.php
+++ lib/config/install.php
@@ -72,3 +72,15 @@ try {
 } catch (Exception $e) {
     waLog::log($e->getMessage());
 }
+
+$utf8mb4 = new tasksUtf8mb4Converter();
+foreach ($utf8mb4->getTables() as $table => $columns) {
+    foreach ($columns as $column) {
+        try {
+            $utf8mb4->convertColumn($table, $column);
+        } catch (Exception $e) {
+            waLog::log($e->getMessage());
+        }
+    }
+}
+
