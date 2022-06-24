bcf7f3ca0227d95a6a47caa0d81e81075eba4666
vofka <vofka@webasyst.com>
2022-06-24 10:10:13 +0300

    tiny ad promoting the Android mobile app

--- lib/classes/Services/tasksTinyAddService.class.php
+++ lib/classes/Services/tasksTinyAddService.class.php
@@ -7,7 +7,7 @@ final class tasksTinyAddService
 {
     private const HIDE_FLAG = 'hide_tiny_ad_until';
     private const HIDE_FOR = '30 days';
-    private const START_DATE = '2022-06-01';
+    private const START_DATE = '2022-06-27';
 
     /**
      * @return array<string, mixed>
@@ -19,46 +19,30 @@ final class tasksTinyAddService
             return [];
         }
 
-        $_webasyst_base_url = (wa()->getLocale() === 'ru_RU')
-            ? 'https://www.webasyst.ru/'
-            : 'https://www.webasyst.com/';
-        $_whichUI = (wa()->whichUI() == '1.3') ? '1' : '2'; //utm
-
         $_tinyAds[] = [
-            'adtype' => 'app',
-            'heading' => _w('Promocode'),
-            'appurl' => $_webasyst_base_url . 'store/app/tasks/?utm_source=statusappwebasyst&utm_medium=inapp_tiny_ad&utm_campaign=1312_inapp_statusappwebasyst_tasksapp_wa' . $_whichUI,
-            'buyurl' => $_webasyst_base_url . 'buy/store/1811/?utm_source=statusappwebasyst&utm_medium=inapp_tiny_ad&utm_campaign=1312_inapp_statusappwebasyst_tasksapp_wa' . $_whichUI,
-            'image' => wa()->getAppStaticUrl() . 'img/tinyad/ad-tasks-app-144.png',
-            'title' => _w('Teamwork'),
-            'subtitle' => _w('When tasks become bigger and more complex.'),
-            'teaser' => _w('Promocode for our flagship Webasyst app.'),
-            'body' => '<strong>' . _w('Our flagship app.') . '</strong>' . ' ' .
-                _w(
-                    'Amazing companion/upgrade for Pocket Lists when it’s time for real collaboration on <em>bigger and more complex tasks</em>. Assignments, task statuses, deadlines, kanban board, more — the app help bringing the order to any complex teamwork.'
-                ),
-            'promocode' => '2EJZB021QE',
-            'discount' => '15',
+            'body' =>
+                'hey there! please see the Sidebar.html for the one and the only Tasks mobile app tiny ad'
         ];
 
-        if (!wa()->appExists('cash')) {
-            $_tinyAds[] = [
-                'adtype' => 'app',
-                'heading' => _w('Promocode'),
-                'appurl' => $_webasyst_base_url . 'store/app/cash/?utm_source=statusappwebasyst&utm_medium=inapp_tiny_ad&utm_campaign=1312_inapp_statusappwebasyst_cashapp_wa' . $_whichUI,
-                'buyurl' => $_webasyst_base_url . 'buy/store/5136/?utm_source=statusappwebasyst&utm_medium=inapp_tiny_ad&utm_campaign=1312_inapp_statusappwebasyst_cashapp_wa' . $_whichUI,
-                'image' => wa()->getAppStaticUrl() . 'img/tinyad/ad-cash-app-144.png',
-                'title' => _w('Cash Flow'),
-                'subtitle' => _w('Forecasts and saves your business money.'),
-                'teaser' => _w('Promocode for managing money the smarter way.'),
-                'body' => '<strong>' . _w('Forecasts and saves your money.') . '</strong>' . ' ' .
-                    _w(
-                        'Shows exact cash on hand balance for any date in the future. This app could have been a <em>life saver</em> for most businesses which did not survive a cash gap because of not knowing it’s coming.'
-                    ),
-                'promocode' => '9ZYNJO6ENP',
-                'discount' => '15',
-            ];
-        }
+        //
+        // if ( 0 ) {
+        //     $_tinyAds[] = [
+        //         'adtype' => 'app',
+        //         'heading' => _w('Promocode'),
+        //         'appurl' => $_webasyst_base_url . 'store/app/cash/?utm_source=statusappwebasyst&utm_medium=inapp_tiny_ad&utm_campaign=1312_inapp_statusappwebasyst_cashapp_wa' . $_whichUI,
+        //         'buyurl' => $_webasyst_base_url . 'buy/store/5136/?utm_source=statusappwebasyst&utm_medium=inapp_tiny_ad&utm_campaign=1312_inapp_statusappwebasyst_cashapp_wa' . $_whichUI,
+        //         'image' => wa()->getAppStaticUrl() . 'img/tinyad/ad-cash-app-144.png',
+        //         'title' => _w('Cash Flow'),
+        //         'subtitle' => _w('Forecasts and saves your business money.'),
+        //         'teaser' => _w('Promocode for managing money the smarter way.'),
+        //         'body' => '<strong>' . _w('Forecasts and saves your money.') . '</strong>' . ' ' .
+        //             _w(
+        //                 'Shows exact cash on hand balance for any date in the future. This app could have been a <em>life saver</em> for most businesses which did not survive a cash gap because of not knowing it’s coming.'
+        //             ),
+        //         'promocode' => '9ZYNJO6ENP',
+        //         'discount' => '15',
+        //     ];
+        // }
 
         //show random tiny
         return $_tinyAds ? $_tinyAds[array_rand($_tinyAds)] : [];
