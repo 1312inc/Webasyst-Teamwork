aece461f9b70e545f61569d744c01fe82afcd4b0
vofka <vofka@webasyst.com>
2022-07-22 17:31:58 +0300

    9.1050: Tasks fails to work unless opened in a browser

--- lib/updates/2/1658495738.php
+++ lib/updates/2/1658495738.php
@@ -2,7 +2,7 @@
 $m = new waModel();
 
 try {
-    $m->exec('alter table tasks_task_log_params modify name varchar(250) not null');
+    $m->exec('alter table tasks_task_log_params modify name varchar(249) not null');
 } catch (waException $exception) {
     waLog::log($exception->getMessage(), 'tasks/update.log');
     waLog::log($exception->getFullTraceAsString(), 'tasks/update.log');
