74ffacbc75fef539256a2b85d5cce8994c7b806c
vofka <vofka@webasyst.com>
2022-07-27 11:10:01 +0300

    2.7.1 version up

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name' => 'Tasks',
     'icon' => 'img/tasks-512.png',
-    'version' => '2.7.0',
+    'version' => '2.7.1',
     'vendor' => '1021997',
     'rights' => true,
     'plugins' => true,
