ee5bed906b63ece9d07f4b762294a29e6d3ed656
Leonix <leonix@motd.ru>
2023-11-17 17:28:11 +0300

    1.2410: user should be able to follow a task even when they don't have rights to modify the task itself

--- lib/classes/Api/Task/Favorite/tasksApiTasksFavoriteHandler.class.php
+++ lib/classes/Api/Task/Favorite/tasksApiTasksFavoriteHandler.class.php
@@ -22,7 +22,7 @@ final class tasksApiTasksFavoriteHandler
         }
 
         $rights = new tasksRights();
-        if (!$rights->canEditTask($task, wa()->getUser())) {
+        if (wa()->getUser()->getId() != $contact->getId() && !$rights->canEditTask($task, wa()->getUser())) {
             throw new tasksAccessException(_w('You cannot edit task'));
         }
 
