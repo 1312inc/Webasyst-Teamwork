3be03342715db20a3b48b1c71b24f1a031a5ddf8
Vladimir V. Tuporshin <vofka@vofka.ru>
2023-12-01 15:35:38 +0300

    v2.9.0 config clean up

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name' => 'Tasks',
     'icon' => 'img/tasks.svg',
-    'version' => '2.9.0', // in development
+    'version' => '2.9.0',
     'vendor' => '1021997',
     'rights' => true,
     'plugins' => true,
