563f0cadc5ae65f0f6995e0b35974d01149210e5
Vladimir V. Tuporshin <vofka@vofka.ru>
2024-05-29 10:51:43 +0300

    1.2694: wa->header async js error issue + wa->headerSingleAppUser() nav

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name' => 'Tasks',
     'icon' => 'img/tasks.svg',
-    'version' => '2.9.3',
+    'version' => '2.9.4',
     'vendor' => '1021997',
     'rights' => true,
     'plugins' => true,
