1eaf13696e3d48d6a9a4fd98009882ab998a413d
Konstantin Zaytsev <zaytsev.const@gmail.com>
2024-06-28 14:17:24 +0300

    1.2700: temporary fix lexical module path

--- js/lexical/index.js
+++ js/lexical/index.js
@@ -1,6 +1,5 @@
 
-
-import('/wa-apps/tasks/js/lexical/lexicalEditor.js').then(({createLexicalInstance}) => {
+import(`${window.static_url}js/lexical/lexicalEditor.js`).then(({createLexicalInstance}) => {
     const demo1 = document.querySelector('#demo1')
     if (demo1) createLexicalInstance(document.querySelector('#demo1'), window.taskLinks || {})
     const demo2 = document.querySelector('#demo2')
