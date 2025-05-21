cdf5886b271bf889e02c86ac989ceabae3083424
moroz <ivan.moroz@webasyst.ru>
2025-05-21 12:26:25 +0300

    #1.2850: tasks, fix PHP 8.2 issues

--- lib/classes/tasksTask.class.php
+++ lib/classes/tasksTask.class.php
@@ -916,6 +916,7 @@ class tasksTask implements ArrayAccess
      * @return boolean true on success or false on failure.
      * The return value will be casted to boolean if non-boolean was returned.
      */
+    #[\ReturnTypeWillChange]
     public function offsetExists($offset)
     {
         return isset($this->data[$offset]) || $this->model->fieldExists($offset) || $this->getStorage($offset) ||
@@ -928,6 +929,7 @@ class tasksTask implements ArrayAccess
      * @param mixed $offset The offset to retrieve.
      * @return mixed Can return all value types.
      */
+    #[\ReturnTypeWillChange]
     public function offsetGet($offset)
     {
         return $this->__get($offset);
@@ -940,6 +942,7 @@ class tasksTask implements ArrayAccess
      * @param mixed $value The value to set.
      * @return void
      */
+    #[\ReturnTypeWillChange]
     public function offsetSet($offset, $value)
     {
         $this->__set($offset, $value);
@@ -951,6 +954,7 @@ class tasksTask implements ArrayAccess
      * @param mixed $offset The offset to unset.
      * @return void
      */
+    #[\ReturnTypeWillChange]
     public function offsetUnset($offset)
     {
         $this->__set($offset, null);
