3c6cbd975ee4660f525ec1ccfdd0e021013f2c70
moroz <ivan.moroz@webasyst.ru>
2025-07-31 12:31:09 +0300

    #1.3006 tasks > @mentions, disable user not can_view

--- lib/actions/tasks/tasksTasksGetUsersForProject.controller.php
+++ lib/actions/tasks/tasksTasksGetUsersForProject.controller.php
@@ -4,15 +4,23 @@ class tasksTasksGetUsersForProjectController extends waJsonController
 {
     public function execute()
     {
+        $rights_info = [];
         $projectId = waRequest::request('project_id', null, waRequest::TYPE_INT);
+        $task_id = waRequest::request('task_id', null, waRequest::TYPE_INT);
+        $users = (new tasksApiTeamGetTopAssigneesHandler())->getUsers(new tasksApiTeamGetTopAssigneesRequest($projectId));
 
-        $this->response = (new tasksApiTeamGetTopAssigneesHandler())
-            ->getUsers(new tasksApiTeamGetTopAssigneesRequest($projectId));
+        if ($task_id) {
+            $tasks = [new tasksTask($task_id)];
+            (new tasksRights())->extendTasksByRightsInfo($tasks, array_column($users, 'id'));
+            $rights_info = ifset($tasks, 0, 'rights_info', []);
+        }
 
-        foreach ($this->response as &$item) {
+
+        foreach ($users as &$item) {
             foreach (['name', 'firstname', 'middlename', 'title', 'company', 'jobtitle', 'about', 'login'] as $toEscape) {
                 $item[$toEscape] = htmlspecialchars((string) $item[$toEscape]);
             }
+            $item['rights_info'] = ifset($rights_info, $item['id'], []);
             if (!empty($item['calendar_status'])) {
                 $item['calendar_status'] = [
                     'name' => $item['calendar_status']->getName(),
@@ -21,5 +29,7 @@ class tasksTasksGetUsersForProjectController extends waJsonController
                 ];
             }
         }
+
+        $this->response = $users;
     }
 }
