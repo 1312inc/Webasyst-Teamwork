79d7256850428b4f4b689064d058154950cfa2d6
moroz <ivan.moroz@webasyst.ru>
2025-07-31 16:07:31 +0300

    #1.3007 tasks > После отмены премиум лицензии все фичи остаются доступными

--- lib/classes/tasksLicensing.class.php
+++ lib/classes/tasksLicensing.class.php
@@ -3,26 +3,6 @@
 /**
  * Class tasksLicensing
  */
-class tasksLicensing
+class tasksLicensing extends waLicensing
 {
-    /**
-     * @return bool
-     * @throws waDbException
-     * @throws waException
-     */
-    public static function isPremium()
-    {
-        $is_premium = wa()->getSetting('license_premium', '', 'tasks');
-        if ($is_premium) {
-            return true;
-        }
-
-        if (waLicensing::check('tasks')->hasPremiumLicense()) {
-            $app_settings = new waAppSettingsModel();
-            $app_settings->set('tasks', 'license_premium', date('Y-m-d H:i:s'));
-            return true;
-        }
-
-        return false;
-    }
 }
