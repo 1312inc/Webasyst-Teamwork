0ed79d6cff4ee3535b525a7fb6a10209f98e0add
Vladimir V. Tuporshin <vofka@vofka.ru>
2025-07-31 21:10:02 +0300

    3.1.0 app version up

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name' => 'Tasks',
     'icon' => 'img/tasks.svg',
-    'version' => '3.0.0',
+    'version' => '3.1.0',
     'vendor' => '1021997',
     'rights' => true,
     'plugins' => true,
