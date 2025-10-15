cb5bfc78e85fbfc64a5429ee3ed28bac4ab2b5f8
Vladimir V. Tuporshin <vofka@vofka.ru>
2025-10-15 11:01:31 +0300

    1.3019: Dev: /upgrade/ promo

--- lib/classes/tasksViewHelper.class.php
+++ lib/classes/tasksViewHelper.class.php
@@ -33,12 +33,12 @@ class tasksViewHelper extends waAppViewHelper
         if (wa()->getLocale() == 'ru_RU')
         {
             $pricing = array( 'compare_price' => '27 999', 'price' => '11 999 <span class="ruble">₽</span>', 'special' => '' );
-            // if (date('Ymd')<='20250731') $pricing = array( 'compare_price' => '11 999', 'price' => '8 399 <span class="ruble">₽</span>', 'special' => '&minus;30% до 31.07', 'special_short' => '&minus;30% / 31.07', 'special_color' => 'orange' );
+            if (date('Ymd')<='20251031') $pricing = array( 'compare_price' => '11 999', 'price' => '8 999 <span class="ruble">₽</span>', 'special' => '&minus;25% до 31.10', 'special_short' => '&minus;25% / 31.10', 'special_color' => 'orange' );
         }
         else
         {
             $pricing = array( 'compare_price' => '$449', 'price' => '$199', 'special' => '' );
-            // if (date('Ymd')<='20250731') $pricing = array( 'compare_price' => '$199', 'price' => '$139', 'special' => '&minus;30%', 'special_short' => '&minus;30% / 07.31', 'special_color' => 'orange' );
+            if (date('Ymd')<='20251031') $pricing = array( 'compare_price' => '$199', 'price' => '$149', 'special' => '&minus;25%', 'special_short' => '&minus;25% / 10.31', 'special_color' => 'orange' );
         }
 
         return $pricing;
